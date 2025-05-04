a024bb1771f7f958775e56c60a18bb8786e00f50
JOSIANE ARAÚJO LOURENÇO <Josianelourenco2000@gmail.com>
2025-05-04 17:01:17 -0300

    Add files via upload
    teste cypress e algumas alterações no HTML e JAVASCRIPT .
    
    obs: Ainda precisa ajustar a questão da transferência

---
+++ cypress-bank/bugBank/index.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded',() =>{
+
+ 
+
+ const form= document.getElementById("loginForm");
+ 
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const senha = document.getElementById("senha").value.trim();
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || {};
+
+   
+
+    if (usuarios[email] && usuarios[email].senha === senha) {
+      localStorage.setItem("usuarioLogado", email)
+      window.location.href= "inicio.html";
+    
+
+    }
+   
+  else {
+    alert("E-mail ou senha incorretos.");
+  }  
+ 
+
+  document.getElementById("cadastroBtn").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    window.location.href = "cadastro.html";
+  })
+  })
+
+});
+
